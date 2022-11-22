e7078e640c108ee6a80d94ae15eb1cc64f5bd1f2
muammarzaki <muammarzaki2004@gmail.com>
2022-11-22 07:14:43 +0700

    membuat list menjadi nomor dan sidikit menambah type di setiap var

--- hashcrack.py
+++ hashcrack.py
@@ -5,42 +5,49 @@ import pyfiglet
 ascii_banner = pyfiglet.figlet_format("crack with kira")
 print (ascii_banner)
 
-print("ALGORITMA:\nMD5\nSHA1\nSHA224\nSHA256\nSHA384\nSHA512")
+print("""ALGORITMA:
+1)MD5
+2)SHA1
+3)SHA224
+4)SHA256
+5)SHA384
+6)SHA512
+""")
 
-tipe_hash = str(input("mau hash tipe apa?:"))
-lokasi_wordlist = str(input("masukkan lokasi wordlist:"))
-hash = str(input("masukkan hash:"))
+tipe_hash:int = int(input("mau hash tipe apa?:"))
+lokasi_wordlist:str = str(input("masukkan lokasi wordlist:"))
+hash:str = str(input("masukkan hash:"))
 
-word_list = open(f"{lokasi_wordlist}").read()
-lists = word_list.splitlines()
+word_list:str = open(f"{lokasi_wordlist}").read()
+lists:list[str] = word_list.splitlines()
 
 for word in lists:
-    if tipe_hash == "MD5" :
+    if tipe_hash == 1 :
         hash_object = hashlib.md5(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
             print(f"\033[1;32m HASH FOUND: {word} \n")
-    elif tipe_hash == "SHA1":
+    elif tipe_hash == 2:
         hash_object = hashlib.sha1(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
             print(f"\033[1;32m HASH FOUND: {word} \n")
-    elif tipe_hash == "SHA224":
+    elif tipe_hash == 3:
         hash_object = hashlib.sha224(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
             print(f"\033[1;32m HASH FOUND: {word} \n")
-    elif tipe_hash == "SHA256":
+    elif tipe_hash == 4:
         hash_object = hashlib.sha256(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
             print(f"\033[1;32m HASH FOUND: {word} \n")
-    elif tipe_hash == "SHA384":
+    elif tipe_hash == 5:
         hash_object = hashlib.sha384(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
             print(f"\033[1;32m HASH FOUND: {word} \n")
-    elif tipe_hash == "SHA512":
+    elif tipe_hash == 6:
         hash_object = hashlib.sha512(f"{word}".encode('utf-8'))
         hashed = hash_object.hexdigest()
         if hash == hashed:
